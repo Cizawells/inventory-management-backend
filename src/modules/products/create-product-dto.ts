92e6bfbbeea03aa48d2f3bcc70816166c0be6510
ciza Wells <cizawells70@gmail.com>
2025-08-27 12:47:17 +0300

    added the Procfile for aws elastic beanstalk  deployment

--- src/modules/products/create-product-dto.ts
+++ src/modules/products/create-product-dto.ts
@@ -1,97 +1,121 @@
-import {
-  IsString,
-  IsOptional,
-  IsEnum,
-  IsNumber,
-  IsBoolean,
-  IsUUID,
-  IsNotEmpty,
-} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 import { ProductType } from '@prisma/client';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUUID,
+} from 'class-validator';
 
 export class CreateProductDto {
+  @ApiProperty({ example: 'Laptop' }) // 👈 shows in Swagger
   @IsString()
   @IsNotEmpty({ message: 'This field can not be empty' })
   name: string;
 
+  @ApiProperty({ example: '123' }) // 👈 shows in Swagger
   @IsString()
   sku: string;
 
+  @ApiProperty({ example: 'BAR444' }) // 👈 shows in Swagger
   @IsOptional()
   @IsString()
   barcode?: string;
 
+  @ApiProperty({ example: 'product description' }) // 👈 shows in Swagger
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiProperty({ example: 'stockable' }) // 👈 shows in Swagger
   @IsEnum(ProductType)
   productType: ProductType;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   categoryId?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   brandId?: string;
 
+  @ApiProperty({ example: '5000' }) // 👈 shows in Swagger
   @IsOptional()
   @IsNumber()
   costPrice?: number;
 
+  @ApiProperty({ example: '8000' }) // 👈 shows in Swagger
   @IsOptional()
   @IsNumber()
   salePrice?: number;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsString()
   currency?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   uomId?: string;
 
+  @ApiProperty({ example: 1 }) // 👈 shows in Swagger
   @IsOptional()
   @IsNumber()
   stockQty?: number;
 
+  @ApiProperty({ example: 1 }) // 👈 shows in Swagger
   @IsOptional()
   @IsNumber()
   reorderLevel?: number;
 
+  @ApiProperty({ example: true }) // 👈 shows in Swagger
   @IsOptional()
   @IsBoolean()
   isActive?: boolean;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   taxCategoryId?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   incomeAccountId?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   expenseAccountId?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   inventoryAccountId?: string;
 
+  @ApiProperty({ example: '1' }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   defaultSupplierId?: string;
 
+  @ApiProperty({ example: 1 }) // 👈 shows in Swagger
   @IsOptional()
   @IsNumber()
   leadTimeDays?: number;
 
+  @ApiProperty({ example: false }) // 👈 shows in Swagger
   @IsOptional()
   @IsBoolean()
   hasVariants?: boolean;
 
+  @ApiProperty({ example: null }) // 👈 shows in Swagger
   @IsOptional()
   @IsUUID()
   parentId?: string;
